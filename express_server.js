e9a16bdcf7a0bbf6573e9f98fb7d630fb2536c12
M-ARTHELL <makoarthell@proton.me>
2023-10-28 12:28:24 -0300

    add registration page

--- express_server.js
+++ express_server.js
@@ -97,6 +97,13 @@ app.post("/logout", (req, res) => {
   res.redirect('/urls');
 })
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("registration", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
