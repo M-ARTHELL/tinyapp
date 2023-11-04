3a9c9bf64066fc7c29703a6c1279109af6abc62d
M-ARTHELL <makoarthell@proton.me>
2023-11-04 17:35:11 -0300

    Add: comments, better organization

--- express_server.js
+++ express_server.js
@@ -1,9 +1,28 @@
+///////////////////////////////////////////////////////////
+// SETUP
+///////////////////////////////////////////////////////////
+
 const { render } = require("ejs");
 const express = require("express");
 const cookieParser = require("cookie-parser")
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// listing for requests on port specified by variable, prints it in console
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+///////////////////////////////////////////////////////////
+// FUNCTIONS
+///////////////////////////////////////////////////////////
+
+// generates a random 6 character string
 function generateRandomString() {
   let result = "";
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -13,13 +32,28 @@ function generateRandomString() {
   return result;
 }
 
-app.set("view engine", "ejs");
+// finds user in user datbase and returns the associated key, returns false if email does not exist
+const findUser = function (email) {
+  for (const user in users) {
 
+    if (users[user].email === email) {
+      return user;
+    }
+  }
+  return false;
+}
+
+///////////////////////////////////////////////////////////
+// DATABASES
+///////////////////////////////////////////////////////////
+
+// urls
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// users
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -33,28 +67,17 @@ const users = {
   },
 }
 
-const findUser = function (email) {
-  for (const user in users) {
+///////////////////////////////////////////////////////////
+// ROUTES
+///////////////////////////////////////////////////////////
 
-    if (users[user].email === email) {
-      return user;
-    }
-  }
-  return false;
-}
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
+// welcome page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
+// url listing
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -67,12 +90,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body["longURL"];
-  res.redirect(`/urls/${shortURL}`);
-});
 
+// new urls
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     user_id: req.cookies["user_id"]
@@ -80,6 +99,14 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body["longURL"];
+  res.redirect(`/urls/${shortURL}`);
+});
+
+
+// url page
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -91,25 +118,33 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls/:id/", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls/")
+  res.redirect("/urls")
 })
 
+
+// delete url
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 })
 
+
+// redirects short link to long link
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id]
 
+  //checks database for short url and redirects to long url if found
   if (urlDatabase[req.params.id]) {
     res.redirect(longURL);
-  } else {
-    res.redirect('/404');
-  }
 
+  } else {
+    // otherwise, sends 403 code and "url not found" message
+    res.status(403).send("URL not found");
+  }
 });
 
+
+// login
 app.get("/login", (req, res) => {
   const templateVars = {
     user_id: req.cookies["user_id"]
@@ -118,34 +153,46 @@ app.get("/login", (req, res) => {
   res.render("login", templateVars);
 })
 
+
+// login process
 app.post("/login", (req, res) => {
 
+  //checks if email/password forms are filled
   if (req.body.email && req.body.password) {
     let userID = findUser(req.body.email);
 
+    // if user doesn't exist in database, send code 403 and "not found" message
     if (userID === false) {
-      res.status(400).send("User does not exist")
+      res.status(403).send("User not found")
       
     } else if (users[userID].email === req.body.email) {
 
+      // if user exists and the email and password match, set user_id cookie to log user in, and redirect to /urls
       if (users[userID].password === req.body.password) {
         res.cookie('user_id', userID)
         res.redirect('/urls')
 
       } else {
-        res.status(400).send("Incorrect password")
+        // if password doesn't match, send 403 code and "incorrect password" message
+        res.status(403).send("Incorrect password")
       }
     }
   } else {
+  // if email or password is blank, send code 400 and "missing input" message
   res.status(400).send("Form missing input")
   }
 })
 
+
+// logout
+// clears user_id cookie and redirects to login page
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.redirect('/urls');
+  res.redirect('/login');
 })
 
+
+// register
 app.get("/register", (req, res) => {
   const templateVars = {
     user_id: req.cookies["user_id"]
@@ -153,34 +200,34 @@ app.get("/register", (req, res) => {
   res.render("registration", templateVars);
 })
 
-app.post("/register", (req, res) => {
-  const randomID = generateRandomString();
 
+// registration process
+app.post("/register", (req, res) => {
+
+  // if email and password forms are filled, check if user already exists in user database
   if (req.body.email && req.body.password) {
     const userID = findUser(req.body.email)
 
+    // if user does not exist in database, generate a random string, set it as the key and user id, and set the email and password provided
     if (userID === false) {
+      const randomID = generateRandomString();
       users[randomID] = {
         id: randomID,
         email: req.body.email,
         password: req.body.password,
       }
+
+      // then, set user_id cookie to log user in, and redirect to /urls
       res.cookie('user_id', randomID)
       res.redirect('/urls')
 
+    // if user is found in database, send code 400 and "email in use" message
     } else if (users[userID].email === req.body.email) {
         res.status(400).send(`Email ${req.body.email} already in use`)
     }
 
+  // if email or password is left blank, send code 400 and "missing input" message
   } else {
     res.status(400).send("Form missing input")
   }
 })
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/404", (req, res) => {
-  res.send("<html><body><h1>404: Not Found</h1></body></html>\n");
-});
